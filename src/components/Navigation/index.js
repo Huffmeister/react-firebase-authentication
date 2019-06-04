b324594aae0a0f52257112bb3cfaf489622e48ca
Jon Huff <HuffJonathan@prahs.com>
2019-06-04 16:02:48 -0500

    Added Firebase and Bootstrap.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    background: papayawhip;
+`;
+
+
 const Navigation = () => (
-    <div>
-        <h1>Navigation</h1>
-    </div>
+    <StyledDiv>
+        <ul>
+            <li>
+                <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+        </ul>
+    </StyledDiv>
 );
 
 export default Navigation;
